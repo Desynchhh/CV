5feb2a1fa9d0c5e878ccf912f9cce4a7aee1cec0
Desynch <ltheguy939@gmail.com>
2023-08-11 12:36:12 +0200

    Started implemented Directus as CMS. Basics are down, but it is buggy as hell

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,46 @@
 // import type { PublicGithubRepo } from "$lib/types";
 import type { PageServerLoad, Actions } from "./$types";
+import "dotenv/config";
+
+export const load: PageServerLoad = async ({ fetch, locals }) => {
+    // try {
+    const API_KEY: string | undefined = process.env.DIRECTUS_API_KEY;
+    const filter = JSON.stringify({
+        translations: {
+            _filter: {
+                languages_code: {
+                    _eq: locals.initLocale || "en-US",
+                },
+            },
+        },
+    });
+    console.log(API_KEY);
+    const response = await fetch(
+        `http://0.0.0.0:8055/items/content_with_image?fields=image,style,translations.title,translations.content&deep=${filter}`,
+        {
+            headers: {
+                Authorization: `Bearer ${API_KEY}`,
+            },
+        }
+    );
+    const data: {
+        data: {
+            image: string;
+            style: "image-text" | "text-image";
+            translations: {
+                title: string;
+                content: string;
+            }[];
+        }[];
+    } = await response.json();
+
+    return {
+        content: data,
+    };
+    // } catch (error) {
+    // console.error(error);
+    // }
+};
 
 // export const load = (async ({ fetch }) => {
 //     const token = process.env.GITHUB_ACCESS_TOKEN;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
     import { t } from '$lib/translations';
     import { Banner, Introduction, EduExperience } from '$lib/components';
+
+    export let data;
+    $: ({content} = data);
+
+    console.log(data);
 </script>
 
 <svelte:head>
@@ -9,6 +14,33 @@
 
 <main>
     <Banner />
+    <section class="tester">
+        {#each content.data as introSection}
+        <div id="intro-background" class="daisy-hero min-h-fit">
+            <div class="daisy-hero-content flex-col lg:flex-row-reverse">
+                <img src={`http://0.0.0.0:8055/assets/${introSection.image}`} class="max-w-sm rounded-lg shadow-2xl" alt="Mikkel"/>
+                <div>
+                    <h1 class="text-5xl font-bold">{introSection.translations[0].title}</h1>
+                    {@html introSection.translations[0].content}
+                </div>
+            </div>
+        </div>
+        {/each}
+    </section>
     <Introduction />
     <EduExperience />
+    <!-- <div class="bg-base-200">
+        <div class="grid grid-cols-2 bg-base-200 gap-2 items-center max-w-xs px-8">
+            <p>Java</p>
+            <progress class="grid- daisy-progress daisy-progress-info w-56" value="0" max="100"></progress>
+            <p>.NET</p>
+            <progress class="daisy-progress daisy-progress-info w-56" value="10" max="100"></progress>
+            <p>Rust</p>
+            <progress class="daisy-progress daisy-progress-info w-56" value="40" max="100"></progress>
+            <p>Python</p>
+            <progress class="daisy-progress daisy-progress-info w-56" value="70" max="100"></progress>
+            <p>JS/TS</p>
+            <progress class="daisy-progress daisy-progress-info w-56" value="100" max="100"></progress>
+        </div>
+    </div> -->
 </main>
